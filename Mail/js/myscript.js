a732c9c38562743bafe62c383ae557cf72bb3e5b
Gabrielecorti01 <gcorti.info@gmail.com>
2021-08-06 16:00:40 +0200

    ESERCIZIO TERMINATO

--- Mail/js/myscript.js
+++ Mail/js/myscript.js
@@ -1,5 +1,15 @@
 
+const elencoMail = ["boolean@index.it", "ciao@ciao.it", "mail.ciao@ciao.it", "buona.giornata@info.it"]
+
 let mailUtente = (prompt("Indica la tua mail per accedere"));
 console.log(mailUtente)
 
-const elencoMail = ["boolean@index.it", "ciao@ciao.it", "mail.ciao@ciao.it", "buona.giornata@info.it"]
+for (var i = 0; i < elencoMail.length; i++){
+    let mailCorrente = elencoMail[i]
+    console.log(mailCorrente, i);
+    if (mailUtente == mailCorrente) {
+        alert("Mail corretta, puoi passare!")
+}
+}
+
+
